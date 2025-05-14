d25221baa9881694846c818fa94f6cb37347857b
abakedjoetato <thry360@hotmail.com>
2025-05-14 00:48:11 +0000

    Clean
    Clean up

--- attached_assets/extracted/Fixes2-main/tests/test_suites/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Test Suites Package for Tower of Temptation PvP Statistics Bot
-
-This package contains test suites for different parts of the bot:
-- SFTP commands
-- Error handling
-- Canvas commands
-- And more
-"""
